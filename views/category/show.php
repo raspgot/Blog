24549f5029b6db644bcec6faab0b493b167b2e5f
gauthier witkowski <gauthierw630@gmail.com>
2019-08-26 14:19:19 +0200

    First commit

---
+++ views/category/show.php
@@ -0,0 +1,5 @@
+<?php require VIEW_PATH . '/layouts/header.php'; ?>
+
+<h1>Ma catégorie</h1>
+
+<?php require VIEW_PATH . '/layouts/footer.php'; ?>
---
+++ views/post/index.php
@@ -0,0 +1,5 @@
+<?php require VIEW_PATH . '/layouts/header.php'; ?>
+
+<h1>Mon Blog</h1>
+
+<?php require VIEW_PATH . '/layouts/footer.php'; ?>
