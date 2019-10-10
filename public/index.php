268641b4425b7df8f917a16895ca301c45f1c662
raspgot <gauthierw630@gmail.com>
2019-10-10 21:09:42 +0200

    Pagination

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ $whoops->register();
 
 $router = new App\Router (dirname(__DIR__) . '/views');
 $router
-    ->get('/blog', 'post/index', 'blog')
+    ->get('/', 'post/index', 'home')
     ->get('/blog/[*:slug]-[i:id]', 'post/show', 'post')
     ->get('/blog/category', 'category/show', 'category')
     ->run();
--- views/post/index.php
+++ views/post/index.php
@@ -7,8 +7,21 @@
     $pdo = new PDO('mysql:dbname=blog;host=127.0.0.1', 'root', '', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
-    
-    $query = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT 12");
+
+    $currentPage = (int)($_GET['page'] ?? 1);
+    if ($currentPage <= 0) {
+        throw new Exception('Numéro de page invalide');
+    }
+
+    $count = $pdo->query('SELECT COUNT(id) FROM post')->fetch(PDO::FETCH_NUM)[0];
+    $perPage = 12;
+    $pages = ceil($count / $perPage);
+    if ($currentPage > $pages) {
+        throw new Exception('Cette page n\'existe pas');
+    }
+
+    $offset = $perPage * ($currentPage -1);
+    $query = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT $perPage OFFSET $offset");
     $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
 ?>
 
@@ -20,4 +33,13 @@
         <?php require 'card.php' ?>
     </div>
     <?php endforeach ?>
+</div>
+
+<div class="d-flex justify-content-between my-4">
+    <?php if ($currentPage > 1): ?>
+        <a href="<?= $router->url('home') ?>?page=<?= $currentPage - 1 ?>" class="btn btn-primary">&#8592; Page précédente</a>
+    <?php endif ?>
+    <?php if ($currentPage < $pages): ?>
+        <a href="<?= $router->url('home') ?>?page=<?= $currentPage + 1 ?>" class="btn btn-primary ml-auto">Page suivante &#8594;</a>
+    <?php endif ?>
 </div>
