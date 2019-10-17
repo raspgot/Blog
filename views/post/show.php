e9a1dcfdefea472d719bdbd8148e89710125ce54
raspgot <gauthierw630@gmail.com>
2019-10-17 22:59:30 +0200

    Article

---
+++ views/post/show.php
@@ -0,0 +1,12 @@
+<?php
+use App\Connexion;
+use App\Model\Post;
+
+$id = (int)$params['id'];
+$slug = $params['slug'];
+
+$pdo = Connexion::getPDO();
+$query = $pdo->prepare('SELECT * FROM post WHERE id = :id');
+$query->execute(['id' => $id]);
+$post = $query->fetchAll(PDO::FETCH_CLASS, post::class)[0];
+dd($post);
